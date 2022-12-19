f68bf40b3c5462683ce90d986c92f951efd40dbb
LiavCohen-a <liavcohen2703@gmail.com>
2022-12-19 15:38:46 +0200

    app architecture

---
+++ tradetrakingapp/src/Components/LoginRegisterComps/ForgotPasswordComp.js
@@ -0,0 +1,10 @@
+import {Link} from 'react-router-dom';
+function ForgotPasswordComp() {
+    return (
+      <div>
+        <Link to={"/ForgotPassword"}>Forgot password?</Link>
+      </div>
+    );
+  }
+  
+export default ForgotPasswordComp;
---
+++ tradetrakingapp/src/Pages/LoginRouter/Pages/ForgotPasswordPage.js
@@ -0,0 +1,12 @@
+
+
+function ForgotPasswordPage() {
+    return (
+      <div className="App">
+          Welcome to ForgotPasswordPage
+      </div>
+    );
+  }
+  
+  export default ForgotPasswordPage;
+  
